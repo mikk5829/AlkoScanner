86349ffa4a1313a74e216cbe60225f9dd7f5e2c9
Mikkel Anderson <s184230@student.dtu.dk>
2020-08-17 23:05:47 +0200

    Initial Commit

---
+++ AlkoScanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlkoScanner
+//
+//  Created by Mikkel Rosenfeldt Anderson on 17/08/2020.
+//  Copyright © 2020 Mikkel Rosenfeldt Anderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
