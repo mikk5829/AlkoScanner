658fcbdc1b87ecbb9127b8109d842a175c844f5b
Mikkel Anderson <s184230@student.dtu.dk>
2020-09-01 20:45:48 +0200

    Updated drink

---
+++ AlkoScanner/Supporting Views/CustomButtons.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButtons.swift
+//  AlkoScanner
+//
+//  Created by Mikkel Rosenfeldt Anderson on 30/08/2020.
+//  Copyright © 2020 Mikkel Rosenfeldt Anderson. All rights reserved.
+//
+
+import SwiftUI
+
+struct CustomButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtons()
+    }
+}
