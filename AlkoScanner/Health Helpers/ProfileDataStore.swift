4f2030c42f5d2a0af35ac71596f98e757a2b43df
Mikkel <s184230@student.dtu.dk>
2021-09-07 23:17:56 +0300

    Starting fresh

--- AlkoScanner/Health Helpers/ProfileDataStore.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ProfileDataStore.swift
-//  AlkoScanner
-//
-//  Created by Mikkel Rosenfeldt Anderson on 21/08/2020.
-//  Copyright © 2020 Mikkel Rosenfeldt Anderson. All rights reserved.
-//
-
-import Foundation
